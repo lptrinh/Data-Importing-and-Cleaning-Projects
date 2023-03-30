2be00b4b81740dadcc3d8607b192f9d8a1cd981f
Trinh Phuong Le <125241615+lptrinh@users.noreply.github.com>
2023-03-30 22:53:26 +0700

    Update Exploring-the-NYC-Airbnb-Market.py

--- Exploring-the-NYC-Airbnb-Market.py
+++ Exploring-the-NYC-Airbnb-Market.py
@@ -1,6 +1,12 @@
+/*
+
+NYC Airbnb Data Analysis
+
+*/
+
+
 #1. Importing the Data
-New York City skyline
-Welcome to New York City (NYC), one of the most-visited cities in the world. As a result, there are many Airbnb listings to meet the high demand for temporary lodging for anywhere between a few nights to many months. In this notebook, we will take a look at the NYC Airbnb market by combining data from multiple file types like .csv, .tsv, and .xlsx.
+Context Welcome to New York City (NYC), one of the most-visited cities in the world. As a result, there are many Airbnb listings to meet the high demand for temporary lodging for anywhere between a few nights to many months. In this notebook, we will take a look at the NYC Airbnb market by combining data from multiple file types like .csv, .tsv, and .xlsx.
 
 
 
